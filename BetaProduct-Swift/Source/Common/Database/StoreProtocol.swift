e96e90a4c593048564a57bc93c533b2422cdedd1
Bumpire24 <miguelfontamillas@gmail.com>
2017-11-07 17:21:38 +0800

    Adding two Pod Files, "Alamo Fire" and "CocoaLumberJack"

--- BetaProduct-Swift/Source/Common/Database/StoreProtocol.swift
+++ BetaProduct-Swift/Source/Common/Database/StoreProtocol.swift
@@ -10,5 +10,8 @@ import Foundation
 
 protocol StoreProtocol {
     func fetchEntries(withEntityName entityName : String, withCompletionBlock completionBlock : @escaping CompletionBlockWithResults) -> Void
-    func fetchEntries(withEntityName entityName : String, withPredicate predicate : NSPredicate?, withSortDescriptors sortDescriptors : [NSSortDescriptor]?, withCompletionBlock completionBlock : @escaping CompletionBlockWithResults) -> Void
+    func fetchEntries(withEntityName entityName : String,
+                        withPredicate predicate : NSPredicate?,
+            withSortDescriptors sortDescriptors : [NSSortDescriptor]?,
+            withCompletionBlock completionBlock : @escaping CompletionBlockWithResults) -> Void
 }
