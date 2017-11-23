ab25e402e8fdcbde8d8084c34baa6217d2d27418
Bumpire24 <miguelfontamillas@gmail.com>
2017-11-23 19:04:06 +0800

    Updated module 'Login' Added Integration Test scheme Updated test module 'Login'

---
+++ Pods/Target Support Files/Pods-BetaProduct-Swift DEV (Integration Tests)/Pods-BetaProduct-Swift DEV (Integration Tests)-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BetaProduct_Swift_DEV__Integration_Tests_ : NSObject
+@end
+@implementation PodsDummy_Pods_BetaProduct_Swift_DEV__Integration_Tests_
+@end
