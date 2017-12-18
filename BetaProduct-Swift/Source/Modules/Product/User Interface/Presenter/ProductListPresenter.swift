cf4df78fa78b8b4479e16dbec7d377e8adbfc4a7
Enrico Boller <email.prof.er@gmail.com>
2017-12-18 18:24:09 +0800

    Committed changes for Product List and Product Detail View

--- BetaProduct-Swift/Source/Modules/Product/User Interface/Presenter/ProductListPresenter.swift
+++ BetaProduct-Swift/Source/Modules/Product/User Interface/Presenter/ProductListPresenter.swift
@@ -11,16 +11,30 @@ import Foundation
 class ProductListPresenter: NSObject, ProductsModuleProtocol, ProductListInteractorOutput {
     var interactor : ProductInteractorInput?
     var productsListView : ProductsListViewProtocol?
+    var productDetailView : ProductDetailViewProtocol?
     
     func gotProducts(_ products: [ProductListItem]) {
+        guard type(of: products) == [ProductListItem].Type.self else {
+            productDetailView?.displayProductInformation(productItem: products as! BaseDisplayItem)
+            return
+        }
+        
         productsListView?.displayProducts(products)
     }
     
     func productListDeleteComplete(wasSuccessful isSuccess: Bool, withMessage message: String) {
-        
+        productsListView?.deleteProductItemFromCollection()
     }
     
     func getAllProducts() {
         interactor?.getProducts()
+    }
+    
+    func removeProductItem(withIndex index: Int) {
+        interactor?.deleteProductByIndex(index)
+    }
+    
+    func getProductItem(atIndex index: Int) {
+        interactor?.getProductDetailByIndex(index)
     }
 }
