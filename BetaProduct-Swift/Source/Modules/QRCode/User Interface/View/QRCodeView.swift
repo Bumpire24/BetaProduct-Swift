6424c4d44132f1d4db8a2ec3b0afc054d3baec08
enricosantosboller <enrico.santos.boller@gmail.com>
2017-12-20 14:47:12 +0800

    Cleaned Up Wireframe Changes for QRCodeWireframe, ProductViewWireframe and ShopCartWireframe

---
+++ BetaProduct-Swift/Source/Modules/QRCode/User Interface/View/QRCodeView.swift
@@ -0,0 +1,26 @@
+//
+//  QRCodeView.swift
+//  BetaProduct-Swift
+//
+//  Created by Enrico Boller on 20/12/2017.
+//  Copyright © 2017 User. All rights reserved.
+//
+
+import UIKit
+
+class QRCodeView: BaseView {
+    
+    @IBOutlet weak var QRScannerView: UIView!
+    @IBOutlet weak var QRScannerInstructions: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
